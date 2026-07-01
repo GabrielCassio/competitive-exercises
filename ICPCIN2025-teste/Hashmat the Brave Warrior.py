8023fb7ba51c77cd929ec4064bdd7516f3d1c72e
Gabriel Cassio <gabriel.cgc.contato@gmail.com>
2026-07-01 12:40:02 -0300

    fix:remove-old-folders

--- ICPCIN2025-teste/Hashmat the Brave Warrior.py
+++
@@ -1,5 +0,0 @@
-import sys
-textInput = sys.stdin.readlines()
-textInput = [[int(j[i][0]), int(j[1])] for i in range(len(textInput)) for j in textInput[i].split(" ")]
-
-print(textInput)
--- ICPCIN2025-teste/missing-number.py
+++
@@ -1,4 +0,0 @@
-numInput = int(input())
-lineInt  = [int(i) for i in input().split(" ")]
-total    = (numInput*(numInput+1))//2
-print(total - sum(lineInt))
