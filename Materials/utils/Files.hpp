ac0a9319cc4ef8f2de0eb1238949078719ed8f5f
Gabriel Cassio <gabriel.cgc.contato@gmail.com>
2026-09-11 10:53:01 -0300

    feat(Materials):adding-algorithms-template

---
+++ Materials/utils/Files.hpp
@@ -0,0 +1,14 @@
+// Class to work with files
+template <typename typeFile>
+class Files {
+    private:
+
+    public:
+        iostream read_txt_files () {
+            return
+        }
+
+        iostram write_txt_files () {
+            return
+        }
+};
