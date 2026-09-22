d4f35875d06f7419082991bab1e550ac5c3bae27
Gabriel Cassio <gabriel.cgc.contato@gmail.com>
2026-09-22 14:02:43 -0300

    feat(rnd-1199):files-contest-1199-div-3

---
+++ Contests/div3/1122/B.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+
+#define endl '\n'
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+
+    int t; cin >> t;
+    while (t--) {
+        long long a, b, c; cin >> a >> b >> c;
+
+    }
+
+
+
+    return 0;
+}
